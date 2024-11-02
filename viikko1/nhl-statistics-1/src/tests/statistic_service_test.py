116f1ef4d55438b30637b00b82dfbb1afd2834d5
Rasmus Viitanen <rasmus.viitanen@helsinki.fi>
2024-11-02 11:40:46 +0200

    NHL-tilastot-ohjelman yksikkötestaus

---
+++ viikko1/nhl-statistics-1/src/tests/statistic_service_test.py
@@ -0,0 +1,36 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub()
+        )
+
+    def test_pelaajan_haku(self):
+        player = self.stats.search("Gretzky")
+        self.assertEqual(player.name, "Gretzky")
+
+    def test_haettua_pelaajaa_ei_olemassa(self):
+        player = self.stats.search("Koivu")
+        self.assertEqual(player, None)
+
+    def test_joukkueen_pelaajat(self):
+        det_players = self.stats.team("DET")
+        self.assertEqual(len(det_players), 1)
+
+    def test_eniten_pisteita(self):
+        top_players = self.stats.top(2)
+        self.assertEqual(len(top_players), 3)
